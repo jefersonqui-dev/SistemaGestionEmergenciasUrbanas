b0c05daada21086384ed41adc7fb703ca6628a54
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2025-04-30 15:25:54 -0500

    Inicio al desarrollo del Reto 2

---
+++ src/main/java/com/devsenior/jquiguantar/Sistema_Gestion_Emergencias_Urbanas/SistemaGestionEmergenciasUrbanas.java
@@ -0,0 +1,13 @@
+package com.devsenior.jquiguantar.Sistema_Gestion_Emergencias_Urbanas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionEmergenciasUrbanas {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionEmergenciasUrbanas.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsenior/jquiguantar/Sistema_Gestion_Emergencias_Urbanas/SistemaGestionEmergenciasUrbanasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.jquiguantar.Sistema_Gestion_Emergencias_Urbanas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionEmergenciasUrbanasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
